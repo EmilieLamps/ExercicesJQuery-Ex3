a914beb3847377aeb0096d20cf41365df3a0fb1b
EmilieLamps <emilie.lamps@gmail.com>
2019-11-04 15:04:00 +0100

    Premier envoi

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('#submit').click(function(){ //J'applique mon événement au bouton
+  $('#text').css('font-family' , 'arial')
+  });
+});
+// utiliser la méthode removeAttr
